59e11f01093222b7343373860c784ccd202980bd
Ben Carter <bcarter@redventures.com>
2015-06-23 23:05:17 -0400

    removed commented out code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,3 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-//register_shutdown_function(function() {
-//        $tmp = sys_get_temp_dir();
-//        $files = glob($tmp . "/WatcherTest/*");
-//        foreach ($files as $file) {
-//            @unlink($file);
-//        }
-//
-//        //rmdir($tmp . "/WatcherTest");
-//    });
